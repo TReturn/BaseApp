5da7a0685a9be0d9f0a57926822583e143b32da7
青柠 <1510912562@qq.com>
2022-06-14 18:00:33 +0800

    1.首页文章UI调整。

--- lib_main/src/main/java/com/example/lib_main/adapter/ArticleAdapter.kt
+++ lib_main/src/main/java/com/example/lib_main/adapter/ArticleAdapter.kt
@@ -1,7 +1,9 @@
 package com.example.lib_main.adapter
 
+import androidx.annotation.DrawableRes
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.viewholder.BaseDataBindingHolder
+import com.example.lib_base.utils.image.GlideUtils
 import com.example.lib_main.R
 import com.example.lib_main.databinding.ItemArticleLayoutBinding
 import com.example.lib_main.model.ArticleDetail
@@ -17,9 +19,19 @@ class ArticleAdapter :
         holder: BaseDataBindingHolder<ItemArticleLayoutBinding>,
         item: ArticleDetail
     ) {
-        holder.dataBinding?.let {
-            it.data = item
-            it.executePendingBindings()
+        holder.run {
+            //绑定dataBinding
+            dataBinding?.let {
+                it.data = item
+                it.executePendingBindings()
+            }
+
+            //随机加载一些头像
+            val list:MutableList<Int> = arrayListOf(R.drawable.ic_fruit_apple,R.drawable.ic_fruit_lemon,
+                R.drawable.ic_fruit_lime)
+            GlideUtils.loadImage(context,list[(0..2).random()],getView(R.id.ivHead))
         }
+
+
     }
 }
